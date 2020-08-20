e89d3f8c5defd95f0af310d65bc615107ddedb9e
Kyle Fredin <kfredin@axcient.com>
2020-08-20 13:10:10 -0600

    format tests

--- test/utils.tests.js
+++ test/utils.tests.js
@@ -1,13 +1,13 @@
-const timekeeper = require('timekeeper');
-const expect = require('chai').expect;
+const timekeeper = require("timekeeper");
+const expect = require("chai").expect;
 
-const utils = require('../lib/utils');
+const utils = require("../lib/utils");
 
-describe('utils', function () {
-  describe('generateInstant', function () {
-    it('should pad the millis appropriately', function () {
+describe("utils", function () {
+  describe("generateInstant", function () {
+    it("should pad the millis appropriately", function () {
       timekeeper.withFreeze(0, () => {
-        expect(utils.generateInstant()).to.equal('1970-01-01T00:00:00.000Z');
+        expect(utils.generateInstant()).to.equal("1970-01-01T00:00:00.000Z");
       });
     });
   });
--- test/xmlhelper.js
+++ test/xmlhelper.js
@@ -1,18 +1,21 @@
-var xmlCrypto = require('xml-crypto');
-var xmldom = require('xmldom');
+var xmlCrypto = require("xml-crypto");
+var xmldom = require("xmldom");
 
-exports.verifySignature = function(assertion, cert) {
+exports.verifySignature = function (assertion, cert) {
   try {
     var doc = new xmldom.DOMParser().parseFromString(assertion);
-    var signature = xmlCrypto.xpath(doc, "/*/*[local-name(.)='Signature' and namespace-uri(.)='http://www.w3.org/2000/09/xmldsig#']")[0];
-    var sig = new xmlCrypto.SignedXml(null, { idAttribute: 'AssertionID' });
+    var signature = xmlCrypto.xpath(
+      doc,
+      "/*/*[local-name(.)='Signature' and namespace-uri(.)='http://www.w3.org/2000/09/xmldsig#']"
+    )[0];
+    var sig = new xmlCrypto.SignedXml(null, { idAttribute: "AssertionID" });
     sig.keyInfoProvider = {
       getKeyInfo: function (key) {
         return "<X509Data></X509Data>";
       },
       getKey: function (keyInfo) {
         return cert;
-      }
+      },
     };
     sig.loadSignature(signature.toString());
     var result = sig.checkSignature(assertion);
@@ -26,119 +29,124 @@ exports.verifySignature = function(assertion, cert) {
     console.log(e);
     return false;
   }
-
 };
 
-exports.getIssuer = function(assertion) {
+exports.getIssuer = function (assertion) {
   var doc = new xmldom.DOMParser().parseFromString(assertion);
-  var issuer = doc.documentElement.getElementsByTagName('saml:Issuer');
+  var issuer = doc.documentElement.getElementsByTagName("saml:Issuer");
   return issuer[0].textContent;
 };
 
-exports.getElementText = function(assertion, elementName) {
+exports.getElementText = function (assertion, elementName) {
   var doc = new xmldom.DOMParser().parseFromString(assertion);
   var element = doc.documentElement.getElementsByTagName(elementName);
   return element[0].textContent;
 };
 
-exports.getDestination = function(response) {
+exports.getDestination = function (response) {
   var doc = new xmldom.DOMParser().parseFromString(response);
-  var destination = doc.documentElement.getAttribute('Destination');
+  var destination = doc.documentElement.getAttribute("Destination");
   return destination;
 };
 
-exports.getInResponseTo = function(response) {
+exports.getInResponseTo = function (response) {
   var doc = new xmldom.DOMParser().parseFromString(response);
-  var destination = doc.documentElement.getAttribute('InResponseTo');
+  var destination = doc.documentElement.getAttribute("InResponseTo");
   return destination;
 };
 
-exports.getStatusCode = function(response) {
+exports.getStatusCode = function (response) {
   var doc = new xmldom.DOMParser().parseFromString(response);
   var status = doc.documentElement
-                  .getElementsByTagNameNS('urn:oasis:names:tc:SAML:2.0:protocol', 'StatusCode')[0]
-                  .getAttribute('Value');
+    .getElementsByTagNameNS(
+      "urn:oasis:names:tc:SAML:2.0:protocol",
+      "StatusCode"
+    )[0]
+    .getAttribute("Value");
   return status;
 };
 
-exports.getStatusMessage = function(response) {
+exports.getStatusMessage = function (response) {
   var doc = new xmldom.DOMParser().parseFromString(response);
-  var message = doc.documentElement
-                  .getElementsByTagNameNS('urn:oasis:names:tc:SAML:2.0:protocol', 'StatusMessage')[0]
-                  .textContent;
+  var message = doc.documentElement.getElementsByTagNameNS(
+    "urn:oasis:names:tc:SAML:2.0:protocol",
+    "StatusMessage"
+  )[0].textContent;
   return message;
 };
 
-exports.getSignatureMethodAlgorithm = function(assertion) {
+exports.getSignatureMethodAlgorithm = function (assertion) {
   var doc = new xmldom.DOMParser().parseFromString(assertion);
   return doc.documentElement
-            .getElementsByTagName('SignatureMethod')[0]
-            .getAttribute('Algorithm');
+    .getElementsByTagName("SignatureMethod")[0]
+    .getAttribute("Algorithm");
 };
 
-exports.getDigestMethodAlgorithm = function(assertion) {
+exports.getDigestMethodAlgorithm = function (assertion) {
   var doc = new xmldom.DOMParser().parseFromString(assertion);
   return doc.documentElement
-            .getElementsByTagName('DigestMethod')[0]
-            .getAttribute('Algorithm');
+    .getElementsByTagName("DigestMethod")[0]
+    .getAttribute("Algorithm");
 };
 
-exports.getIssueInstant = function(assertion) {
+exports.getIssueInstant = function (assertion) {
   var doc = new xmldom.DOMParser().parseFromString(assertion);
-  return doc.documentElement.getAttribute('IssueInstant');
+  return doc.documentElement.getAttribute("IssueInstant");
 };
 
 /**
  * @param {String} assertion
  * @return {number} the instant in milliseconds since the Epoch
  */
-exports.getIssueInstantUTC = function(assertion) {
+exports.getIssueInstantUTC = function (assertion) {
   return new Date(exports.getIssueInstant(assertion)).getTime();
 };
 
-exports.getConditions = function(assertion) {
+exports.getConditions = function (assertion) {
   var doc = new xmldom.DOMParser().parseFromString(assertion);
-  return doc.documentElement.getElementsByTagName('saml:Conditions');
+  return doc.documentElement.getElementsByTagName("saml:Conditions");
 };
 
-exports.getConsent = function(assertion) {
+exports.getConsent = function (assertion) {
   var doc = new xmldom.DOMParser().parseFromString(assertion);
-  return doc.documentElement.getAttribute('Consent');
+  return doc.documentElement.getAttribute("Consent");
 };
 
-exports.getAudiences = function(assertion) {
+exports.getAudiences = function (assertion) {
   var doc = new xmldom.DOMParser().parseFromString(assertion);
   return doc.documentElement
-            .getElementsByTagName('saml:Conditions')[0]
-            .getElementsByTagName('saml:AudienceRestriction')[0]
-            .getElementsByTagName('saml:Audience');
+    .getElementsByTagName("saml:Conditions")[0]
+    .getElementsByTagName("saml:AudienceRestriction")[0]
+    .getElementsByTagName("saml:Audience");
 };
 
-exports.getAttributes = function(assertion) {
+exports.getAttributes = function (assertion) {
+  var doc = new xmldom.DOMParser().parseFromString(assertion);
+  return doc.documentElement.getElementsByTagName("saml:Attribute");
+};
+
+exports.getNameIdentifier = function (assertion) {
+  var doc = new xmldom.DOMParser().parseFromString(assertion);
+  return doc.documentElement.getElementsByTagName("saml:NameID")[0];
+};
+
+exports.getNameIdentifierFormat = function (assertion) {
   var doc = new xmldom.DOMParser().parseFromString(assertion);
   return doc.documentElement
-            .getElementsByTagName('saml:Attribute');
+    .getElementsByTagName("NameID")[0]
+    .getAttribute("Format");
 };
 
-exports.getNameIdentifier = function(assertion) {
+exports.getSubjectConfirmationData = function (assertion) {
   var doc = new xmldom.DOMParser().parseFromString(assertion);
-  return doc.documentElement
-            .getElementsByTagName('saml:NameID')[0];
+  return doc.documentElement.getElementsByTagName(
+    "saml:SubjectConfirmationData"
+  )[0];
 };
 
-exports.getNameIdentifierFormat = function(assertion) {
+exports.getAuthnContextClassRef = function (assertion) {
   var doc = new xmldom.DOMParser().parseFromString(assertion);
-  return doc.documentElement
-            .getElementsByTagName('NameID')[0]
-            .getAttribute('Format');
-};
-
-exports.getSubjectConfirmationData = function(assertion) {
-  var doc = new xmldom.DOMParser().parseFromString(assertion);
-  return doc.documentElement.getElementsByTagName('saml:SubjectConfirmationData')[0];
-};
-
-exports.getAuthnContextClassRef = function(assertion) {
-  var doc = new xmldom.DOMParser().parseFromString(assertion);
-  return doc.documentElement.getElementsByTagName('saml:AuthnContextClassRef')[0];
+  return doc.documentElement.getElementsByTagName(
+    "saml:AuthnContextClassRef"
+  )[0];
 };
